e48372fc23a822051b28058828c01dbf2829fad9
EdoardoFornelli <edo.fornelli@stud.uniroma3.it>
2024-06-23 12:20:51 +0200

    scheletro progetto
    oggi si comincia daidaidai

---
+++ src/main/java/it/uniroma3/siw/siwbook/SiwBookApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siwbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiwBookApplication.class, args);
+    }
+
+}
